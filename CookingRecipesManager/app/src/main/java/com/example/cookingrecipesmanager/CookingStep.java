4b6784d21775b3d2af24ffcda7245f66d7093927
Tenrousa Athena <athenaabcxyz@gmail.com>
2023-04-13 17:21:58 +0700

    Complete recipe tracker module

--- CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/CookingStep.java
+++ CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/CookingStep.java
@@ -1,19 +1,17 @@
 package com.example.cookingrecipesmanager;
 
 public class CookingStep {
-    public String stepName;
-    public String stepDetailInstruction;
+    public String stepIntruction;
 
     //stepType values is only one of [Timer, Basic, Prepare]
     public String stepType;
     //Only "Timer" stepType have timeCounter. Other stepType's timer must set to 0
     public int timerBySecond;
 
-    public CookingStep(String stepName, String stepDetailInstruction, String stepType, int timerBySecond) {
-        this.stepName = stepName;
-        this.stepDetailInstruction = stepDetailInstruction;
+    public CookingStep(String stepIntruction, String stepType, int timerBySecond) {
+        this.stepIntruction=stepIntruction;
         this.stepType = stepType;
-        if (stepType.equals("Timer")) {
+        if (!stepType.equals("Timer")) {
             timerBySecond = 0;
         } else {
             this.timerBySecond = timerBySecond;
--- CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/recipetracker/RecipeStepPreview.java
+++ CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/recipetracker/RecipeStepPreview.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.cookingrecipesmanager.CookingStep;
 import com.example.cookingrecipesmanager.R;
@@ -17,12 +18,12 @@ import java.util.ArrayList;
 public class RecipeStepPreview extends AppCompatActivity {
 
     RecyclerView stepList;
-    RecyclerView tagList;
 
+    TextView textView;
     LinearLayoutManager linearLayoutManagerForStep;
-    LinearLayoutManager linearLayoutManagerForTag;
+
     StepListAdapter stepListAdapter;
-    TagListAdapter tagListAdapter;
+
     RecipeDetail recipe;
 
     @Override
@@ -30,20 +31,21 @@ public class RecipeStepPreview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step_preview);
         stepList=findViewById(R.id.stepList);
-        tagList=findViewById(R.id.tagList);
+        textView=findViewById((R.id.recipeName));
 
         //Create data sample for testing
         recipe = new RecipeDetail();
         ArrayList<CookingStep> cookingSteps = new ArrayList<CookingStep>();
-        cookingSteps.add(new CookingStep("Prepare Ingredient", "Prepare 4 cups of water.", "Prepare", 0));
-        cookingSteps.add(new CookingStep("Boil water", "Pour 4 cups of water into a pot and boil it for 2 minutes.", "Timer", 120));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
-        cookingSteps.add(new CookingStep("Take the boiled water out", "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Prepare 4 cups of water.", "Prepare", 0));
+        cookingSteps.add(new CookingStep( "Pour 4 cups of water into a pot and boil it for 2 minutes.", "Timer", 10));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
+        cookingSteps.add(new CookingStep( "Turn of the stove and pour the hot water into 4 cups.", "Basic", 0));
         recipe.CreateStepList(cookingSteps);
+        recipe.recipeName="Boiled water";
         ArrayList<String> tagList = new ArrayList<String>();
         tagList.add("Boil");
         tagList.add("Water");
@@ -55,11 +57,8 @@ public class RecipeStepPreview extends AppCompatActivity {
         stepListAdapter = new StepListAdapter(RecipeStepPreview.this, recipe.cookingStepsList);
         stepList.setLayoutManager(linearLayoutManagerForStep);
         stepList.setAdapter(stepListAdapter);
+        textView.setText(recipe.recipeName);
 
-        linearLayoutManagerForTag=new LinearLayoutManager(RecipeStepPreview.this, LinearLayoutManager.HORIZONTAL, false);
-        tagListAdapter=new TagListAdapter(RecipeStepPreview.this, recipe.tagList);
-        this.tagList.setLayoutManager((linearLayoutManagerForTag));
-        this.tagList.setAdapter(tagListAdapter);
 
     }
 }
