ff2aed87bd8b859c34c630985a877f69ea6b9821
huutai <truongtai137@gmail.com>
2023-05-07 19:44:54 +0700

    Tags: tweak text & img, click to search

--- CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/home/Adapter/TagAdapter.java
+++ CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/home/Adapter/TagAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cookingrecipesmanager.home.Adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cookingrecipesmanager.MainActivity;
 import com.example.cookingrecipesmanager.R;
+import com.example.cookingrecipesmanager.Search;
 import com.example.cookingrecipesmanager.Tag;
 
 import java.util.List;
@@ -30,13 +33,26 @@ public class TagAdapter extends RecyclerView.Adapter<TagAdapter.TagViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull TagViewHolder holder, int position) {
-//        Tag tag = tagList.get(position);
-//        if(tag == null){
-//            return;
-//        }
-//
-//        holder.name.setText(tag.getName());
-//        holder.img.setImageResource(tag.getImg());
+        Tag tag = tagList.get(position);
+        if(tag == null){
+            return;
+        }
+
+        holder.name.setText(tag.getName());
+        if (tag.getImg() != 0) {
+            holder.img.setImageResource(tag.getImg());
+        } else {
+            holder.img.setVisibility(View.GONE);
+        }
+
+        holder.root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Search.class);
+                intent.putExtra("query", tag.getName());
+                v.getContext().startActivity(intent);
+            }
+        });
 //        holder.evaluate.setText(note.getEvaluate().toString());
     }
 
@@ -49,11 +65,13 @@ public class TagAdapter extends RecyclerView.Adapter<TagAdapter.TagViewHolder> {
     }
 
     public class TagViewHolder extends RecyclerView.ViewHolder {
+        private View root;
         private TextView name;
         private ImageView img;
 
         public TagViewHolder(@NonNull View itemView) {
             super(itemView);
+            root = itemView;
             name = itemView.findViewById(R.id.textName) ;
             img = itemView.findViewById(R.id.imageIcon);
 
