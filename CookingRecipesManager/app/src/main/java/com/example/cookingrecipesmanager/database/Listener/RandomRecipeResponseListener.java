4f56449303e7da7a694ab37ec071d54284bde9bd
Tenrousa Athena <athenaabcxyz@gmail.com>
2023-07-20 11:58:05 +0700

    Clean up code. Release APK version.

--- CookingRecipesManager/app/src/main/java/com/example/cookingrecipesmanager/database/Listener/RandomRecipeResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.cookingrecipesmanager.database.Listener;
-
-import com.example.cookingrecipesmanager.database.Model.RandomRecipeAPIResponse;
-
-public interface RandomRecipeResponseListener {
-    void didFetch(RandomRecipeAPIResponse response, String message);
-    void didError(String message);
-}
